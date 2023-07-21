4b63ea7563543830c6e05c63ab08d45eab28b445
Habtetsega <kido3871@gmail.com>
2023-07-20 18:54:15 -0700

    Copy - Cut - Paste

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3="Hol"
+word_last_2="on"
+middle_word="olberto"
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
+

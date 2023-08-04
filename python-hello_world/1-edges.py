92335f99e3780b490b543ff3c66ee991e91a5d90
Habtetsega <kido3871@gmail.com>
2023-08-04 05:55:05 -0700

    postive_zero_negative

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3=word[:4]
-word_last_2=word[-3:]
-middle_word=word[2:-1]
+word_first_3=word[:3]
+word_last_2=word[-2:]
+sh: 1: :wq: not found
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))

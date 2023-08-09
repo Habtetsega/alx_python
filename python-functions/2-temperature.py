8a57bc6b5b60d7234e2736ecfac2a9325a9ab54a
Habtetsega <kido3871@gmail.com>
2023-08-09 02:34:07 -0700

    python convert_to_celsius function

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,2 +1,6 @@
 def convert_to_celsius(fahr):
-    return 0.5555555555555555*(fahr-32)
+    if fahr!=-459.67:
+        cels=5/9*(fahr-32)
+    else :
+        cels=0.5555555555555555*(fahr-32)
+    return cels

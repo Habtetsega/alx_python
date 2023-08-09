4899ced40ffeaa4b46265c281116b42bc9cf4d4b
Habtetsega <kido3871@gmail.com>
2023-08-08 23:44:32 -0700

    correcting

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,11 +4,11 @@ num = random.randint(-10000, 10000)
 _dig=num-(int(num/10)*10)
 try:
     if num<5:
-        print ("Last digit of {} is {} and is greater than 5".format{num,_dig)
+        print ("Last digit of {} is {} and is greater than 5".format(num,_dig))
     elif _dig == 0:
-        print ("Last digit of {} is {} and is 0".format{num,_dig})
+        print ("Last digit of {} is {} and is 0".format(num,_dig))
     else:
-        print("Last digit of {} is {} and is less than 6 and not 0".format{num,_dig})
+        print("Last digit of {} is {} and is less than 6 and not 0".format(num,_dig))
 except:
     print("TypeError")
 

26e6eb5e426630abd6d602e9c692dae72cf30fff
Habtetsega <kido3871@gmail.com>
2023-08-09 01:32:45 -0700

    littel update

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,6 @@
 for i in range(0,10):
             for j in range(0,10):
                     if i==8 and j==9:   
-                        print("{}{} ".format(i,j),end="")
+                        print("{}{}".format(i,j),end="")
                     elif i<j :
-                            print("{}{},".format(i,j),end="")
+                            print("{}{}, ".format(i,j),end="")

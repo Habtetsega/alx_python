fba52d7b3ab046ccc3c5035c848915d6909101dc
Habtetsega <kido3871@gmail.com>
2023-08-10 07:31:19 -0700

    import add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
-python-import_modules
+#!/usr/bin/python3
 from add_0.py import add
 if _name_==_main_:
     a=1
     b=2
-    print("{}+{}={}".format(a,b,add(a,b)))
+    print("{} + {} = {}".format(a,b,add(a,b)))

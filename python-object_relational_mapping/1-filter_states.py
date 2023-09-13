9943e400fd35b21389189570170360bc7f16ae1c
Habtetsega <kido3871@gmail.com>
2023-09-13 03:14:21 -0700

    PEP8 validation

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-import MySQLdb
+"""Module that lists all states from the hbtn_0e_0_usa database."""
 import sys
+import MySQLdb
 
 
-def list_N(username, password, database):
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' AND name NOT LIKE 'n%' ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+if __name__ == "__main__":
+    # Get MySQL credentials from command-line arguments
+    # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    list_N(username, password, database)
+    # Execute the SQL query to retrieve all states sorted by id
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+

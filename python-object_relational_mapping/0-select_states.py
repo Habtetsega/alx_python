c153502b473f0d182c8dc3aa7ee86ff6adae2074
Habtetsega <kido3871@gmail.com>
2023-09-12 23:47:55 -0700

    script that lists all states from the database hbtn_0e_0_usa

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_all_states(username,password,database):
+    db=MYSQLDB.connect(host='localhost',/ port=3306,/username=username,/pwd=password,/db=database)
+    cursor=db.cursor()
+    cursor.execut("SELECT * FROM states ORDER BY ASC")
+    rows=cursor.fechall()
+    for row in rows:
+        print(row)
+    db.close()
+
+
+if _name_="_main_":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
+
+
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+python ORM projects

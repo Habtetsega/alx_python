28693b0ab8e9173878648c7e6485b84f29ee245f
Habtetsega <kido3871@gmail.com>
2023-09-23 03:32:44 -0700

    importing modules

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
-if _name_ == "_main" : 
+if _name_ == "_main_" : 
     import sys
     c = len(sys.args)-1
     if c == 0:
         print("0 argument.")
     else :
         print("{} arguments.\n".format(c))
-        for i in range(1:c+1):
-            print("{}: {} \n".format(i,sys.args[i]))
+        for i in range(c):
+            print("{}: {} \n".format(i+1,sys.args[i+1]))

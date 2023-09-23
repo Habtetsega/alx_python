c4750ebef3185ccdedca37023fcb931b7f121f42
Habtetsega <kido3871@gmail.com>
2023-09-23 06:11:24 -0700

    matrix printing

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,6 +1,6 @@
 def no_c(my_string):
     new_string = ""
     for i in my_string:
-        if i!="c" or "C":
+        if i!="c" or i!= "C":
             new_string+=i
     return (new_string)
---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,7 @@
+def print_matrix_integer(matrix=[[]]):
+    r=len(matrix)Height
+    c=len(matrix)Width
+    for i in range(r):
+        j=0
+        print("{}{}{}\n".format(matrix[i][j],matrix[i][j+1],matrix[i][j+2]))
+        j++

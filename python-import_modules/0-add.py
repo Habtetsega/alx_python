db196a9c7d568beb4c7408db1343ecfc42e9183b
Habtetsega <kido3871@gmail.com>
2023-09-23 02:53:49 -0700

    importing add module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-if _name_=="_main_":
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
     from add_0 import add
-    a=1
-    b=2
-    print("{} + {} = {}".format(a,b,add(a,b)))
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+

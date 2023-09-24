e736130b693fd7537f1610d09bf99009c5f1fe6a
Habtetsega <kido3871@gmail.com>
2023-09-24 01:35:27 -0700

    python data structure

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     c=len(matrix[0])
     for i in range(r):
         j=0
-        print("{:d}{:d}{:d}\n".format(matrix[i][j],matrix[i][j+1],matrix[i][j+2]))
+        print("{:d} {:d} {:d}".format(matrix[i][j],matrix[i][j+1],matrix[i][j+2]))
         j+=1
--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,4 +1,6 @@
 def multiple_returns(sentence):
     l=len(sentence)
     f=sentence[0]
+    if l==0 and f==0:
+        f=None
     return l,f

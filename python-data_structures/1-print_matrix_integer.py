80bba3d518193aa9b6e491e4771ecb7de7a67dec
Habtetsega <kido3871@gmail.com>
2023-09-24 01:24:54 -0700

    python data structure

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(r):
         j=0
         print("{:d}{:d}{:d}\n".format(matrix[i][j],matrix[i][j+1],matrix[i][j+2]))
-        j++
+        j+=1

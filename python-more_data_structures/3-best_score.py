be76a1be717c405cd032d22faf1acff5dde76512
Habtetsega <kido3871@gmail.com>
2023-09-24 05:27:36 -0700

    score and map

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,5 +1,13 @@
 def best_score(a_dictionary):
-    if len(a_dictionary)==0:
+    if not a_dictionary:
         return None
-    M = max(a_dictionary,lambda k:a_dictionary[k])
-    return M
+    
+    max_key = None
+    max_value = float('-inf')
+    
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_key = key
+            max_value = value
+    
+    return max_key

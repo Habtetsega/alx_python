cc9ae12e6bc4f7ae4b52c5d81d904c0962d29610
Habtetsega <kido3871@gmail.com>
2023-09-24 05:20:18 -0700

    best to best

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,7 +1,5 @@
 def best_score(a_dictionary):
     if len(a_dictionary)==0:
         return None
-    M = max(a_dictionary)
-    for key,val in a_dictionary:
-        if val == M:
-            return key
+    M = max(a_dictionary,lambda k:a_dictionary[k])
+    return M

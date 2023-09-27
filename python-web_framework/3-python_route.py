e6ec404ff5f3e22c8ea8c319565d62747ec759d0
Habtetsega <kido3871@gmail.com>
2023-09-27 02:17:09 -0700

    hello 3_python

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -1,37 +1,21 @@
-"""
-Import Flask From flask
-
-"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-"""
-display 'Hello HBNB!'
-"""
 def hello_hbnb():
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
-"""
-display 'HBNB'
-"""
 def hbnb():
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
-"""
-display 'C <text>'
-"""
 def ctext(text):
     new_text = text.replace("_", " ")
     return ("C {}".format(next_text))
 
 @app.route('/python/<text>', defaults={"text":"is cool"}, strict_slashes=False)
-"""
-display 'Python <text>'
-"""
 def pytext(text="is cool"):
     new_text = text.replace("_", " ")
     return ("Python {}".format(new_text))

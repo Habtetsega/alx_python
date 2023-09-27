d769ed8fca46fc723577d331fda44bd2d08ccec1
Habtetsega <kido3871@gmail.com>
2023-09-27 01:06:13 -0700

    readme and hello hbnb

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""importing Flask"""
 from flask import Flask
 app = Flask(__name__)
 @app.route("/", strict_slashes=False)
+"""Difining hello fun"""
 def hello_hbnb:
+    """display Hello HBNB"""
     return("Hello HBNB!")
 if __name__ = "__main__":
     app.run(host='0.0.0.0', port=5000)

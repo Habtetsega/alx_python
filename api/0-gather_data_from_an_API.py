90788863373519b005f731368a4fd2f3dd1db352
Habtetsega <kido3871@gmail.com>
2023-10-04 02:58:11 -0700

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -31,7 +31,6 @@ def display_todo_progress(employee_id):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])

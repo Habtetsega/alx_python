82bd1d1f763bf118fa6ce820f5f14b3ac22040b9
Habtetsega <kido3871@gmail.com>
2023-10-09 07:12:32 -0700

    Documentation

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,12 +1,44 @@
+"""
+Flask Web Application
+
+This module contains a simple Flask web application with two routes: "/" and "/hbnb".
+
+Routes:
+    /: Displays "Hello HBNB!"
+    /hbnb: Displays "HBNB"
+
+Usage:
+    $ python app.py
+
+    The web application will start and listen on 0.0.0.0:5000.
+    Visit http://localhost:5000/ to see the message "Hello HBNB!".
+    Visit http://localhost:5000/hbnb to see the message "HBNB".
+
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    return 'Hello HBNB!'
-@app.route('/hbnb', strict_slashes=False)
+    """
+    Displays "Hello HBNB!".
+
+    Returns:
+        str: The message "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """
+    Displays "HBNB".
+
+    Returns:
+        str: The message "HBNB".
+    """
     return "HBNB"
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

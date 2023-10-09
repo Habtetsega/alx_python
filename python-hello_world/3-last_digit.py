48d73c43b316ed65708e4dac9cc59a0eb3490594
Habtetsega <kido3871@gmail.com>
2023-10-09 06:41:57 -0700

    python last digit and comb3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,7 +8,7 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
 # Print the result based on the value of the last digit
-print("The string Last digit of", number, "is", last_digit, end=" ")
+print("Last digit of", number, "is", last_digit, end=" ")
 
 if last_digit > 5:
     print("and is greater than 5")

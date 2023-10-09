ea20315ac1f2ddabb7ff7af91075e138e5d32ec0
Habtetsega <kido3871@gmail.com>
2023-10-09 06:36:58 -0700

    python last digit and comb3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 import random
-num = random.randint(-10000, 10000)
-_dig=num-(int(num/10)*10)
-try:
-    if num<5:
-        print ("Last digit of {} is {} and is greater than 5".format(num,_dig))
-    elif _dig == 0:
-        print ("Last digit of {} is {} and is 0".format(num,_dig))
-    else:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(num,_dig))
-except:
-    print("TypeError")
 
+# This line should not be modified
+number = random.randint(-10000, 10000)
+
+# Extract the last digit of the number using modulus operator (%)
+last_digit = abs(number) % 10
+
+# Print the result based on the value of the last digit
+print("The string Last digit of", number, "is", last_digit, end=" ")
+
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,5 @@
-for i in range(0,10):
-            for j in range(0,10):
-                    if i==8 and j==9:   
-                        print("{}{}".format(i,j),end="")
-                    elif i<j :
-                            print("{}{}, ".format(i,j),end="")
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=", ")
+
+print()

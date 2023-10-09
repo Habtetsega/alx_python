1110f72de2dcfdb42e8ca6f9975aae1e6e7132ab
Habtetsega <kido3871@gmail.com>
2023-10-09 06:49:02 -0700

    89

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,5 @@
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=", " if j < 9 else "")
+        print("{:d}{:d}".format(i, j), end=", " if j!=9 and i!=8  else "")
 
 print()
